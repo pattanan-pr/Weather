acb2c167b6ba4c72d9798d02a9ddb9cf2195c5ac
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2023-03-31 06:18:05 +0700

    decoding api

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,51 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weathermanager = Weathermanager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+        weathermanager.delegate = self
     }
 
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }else {
+            textField.placeholder = "You should enter something!"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let cityname = searchTextField.text{
+            weathermanager.fetchWeather(cityName: cityname)
+        }
+        searchTextField.text = ""
+    }
+    func didUpdateView(_ weatherManager: Weathermanager, weather: WeatherModel){
+        print(weather.temperature)
+    }
+    func didFail(error: Error) {
+        print("Something went wrong!")
+    }
 }
 
--- Clima/Models/Weathermanager .swift
+++ Clima/Models/Weathermanager .swift
@@ -1,9 +1,63 @@
 //
-//  Weathermanager .swift
+//  WeatherManager.swift
 //  Clima
 //
-//  Created by Pattanan Prarom on 31/3/2566 BE.
+//  Created by Pattanan Prarom on 29/3/2566 BE.
 //  Copyright © 2566 BE App Brewery. All rights reserved.
 //
 
 import Foundation
+
+protocol WeatherManagerDelegate{
+    func didUpdateView(_ weatherManager: Weathermanager, weather: WeatherModel)
+    func didFail(error: Error)
+}
+
+struct Weathermanager {
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?&appid=3a17de987e3d08b15d6eebd25e005912&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(cityName: String){
+        let urlstr = "\(weatherUrl)&q=\(cityName)"
+        performrequest(urlstr: urlstr)
+    }
+    
+    func performrequest(urlstr: String) {
+        if let url = URL(string: urlstr) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    delegate?.didFail(error: error!)
+                }
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData){
+                        delegate?.didUpdateView(self, weather: weather)
+                        //                        let weatherViewCont = WeatherViewController()
+                        //                        weatherViewCont.didUpdateView(weather: weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    func parseJSON(_ weatherData: Data) -> WeatherModel?{
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let name = decodedData.name
+            let temp = decodedData.main.temp
+            
+            let weather = WeatherModel(weatherid: id, cityName: name, temperature: temp)
+            
+            print(weather.toOneDecimal)
+            return weather
+        }catch{
+            delegate?.didFail(error: error)
+            return nil
+        }
+    }
+    
+}
+
